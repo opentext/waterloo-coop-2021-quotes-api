87c059ab233d42261c9ed7268800050fe4e42976
Jason Ibrahim <jasonaibrahim@gmail.com>
2021-01-25 10:21:11 -0800

    initial commit

---
+++ src/main/java/com/opentext/waterloo/quotesapi/QuotesApiApplication.java
@@ -0,0 +1,13 @@
+package com.opentext.waterloo.quotesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuotesApiApplication.class, args);
+    }
+
+}

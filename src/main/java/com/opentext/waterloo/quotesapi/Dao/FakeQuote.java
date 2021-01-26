1468dd2cebfd666b7b173c2be1fb0fd598f1ace5
rachell <rachell@opentext.com>
2021-01-26 15:44:17 -0500

    Added the get method

--- src/main/java/com/opentext/waterloo/quotesapi/Dao/FakeQuote.java
+++ src/main/java/com/opentext/waterloo/quotesapi/Dao/FakeQuote.java
@@ -1,5 +1,7 @@
 package com.opentext.waterloo.quotesapi.Dao;
 
+import com.opentext.waterloo.quotesapi.model.Quote;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,8 +10,13 @@ public class FakeQuote implements QuotesDao{
     private static List<Quote> DB = new ArrayList<>();
 
     @Override
-    public int putQuote(Quote current) {
-        DB.add(new Quote(quote.getId(), quote.getText, quote.getDate);
+    public int putQuote(Quote quote) {
+        DB.add(new Quote(quote.getId(), quote.getText(), quote.getDate()));
         return 1;
+    }
+
+    @Override
+    public List<Quote> selectAllQuote() {
+        return DB;
     }
 }

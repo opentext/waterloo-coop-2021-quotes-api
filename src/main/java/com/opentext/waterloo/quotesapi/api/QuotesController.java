dabb08fe28a304af33a2ccae1ea1c3bee90c41b2
a2zain <a2zain@uwaterloo.ca>
2021-01-26 15:15:38 -0500

    Controller update

--- src/main/java/com/opentext/waterloo/quotesapi/api/QuotesController.java
+++ src/main/java/com/opentext/waterloo/quotesapi/api/QuotesController.java
@@ -2,11 +2,35 @@ package com.opentext.waterloo.quotesapi.api;
 
 import com.opentext.waterloo.quotesapi.model.Quotes;
 import com.opentext.waterloo.quotesapi.service.QuotesService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RequestMapping("api/v1/quotes")
 @RestController
 public class QuotesController {
+    private final QuotesService quotesService;
 
+    @Autowired
+    public QuotesController(QuotesService quotesService) {
+        this.quotesService = quotesService;
+    }
+
+    @PostMapping
+    public void addPerson(@RequestBody Person person) {
+        personService.addPerson(person);
+    }
+
+    @GetMapping
+    public List<Person> getAllPeople() {
+        return personService.getAllPeople();
+    }
+
+    @GetMapping(path = "{id}")
+    public Person getPersonById(@PathVariable("id") UUID id) {
+        return personService.getPersonById(id)
+                .orElse(null);
+    }
 }

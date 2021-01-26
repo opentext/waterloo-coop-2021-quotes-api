1212649c73d9f264f3eb39fc5d8d456cb9653470
ketan <ketan@opentext.com>
2021-01-26 15:17:18 -0500

    QuoteService update

--- src/main/java/service/QuoteService.java
+++ src/main/java/service/QuoteService.java
@@ -1,10 +1,14 @@
 package service;
 
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
 public class QuoteService {
     private final QuoteDao quoteDao;
 
+    public QuoteService (@Qualifier ("postgres") QuoteDao quoteDao){
+        this.quoteDao = quoteDao;
+    }
 }

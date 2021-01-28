11c995a000eccba9271c9f6a152a87397bec5547
ketan <ketan@opentext.com>
2021-01-27 22:01:08 -0500

    moved datasource into api file

--- src/main/java/com/opentext/waterloo/quotesapi/datasource/PostgresDatasource.java
+++ src/main/java/com/opentext/waterloo/quotesapi/datasource/PostgresDatasource.java
@@ -1,4 +1,4 @@
-package datasource;
+package com.opentext.waterloo.quotesapi.datasource;
 
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.boot.context.properties.ConfigurationProperties;

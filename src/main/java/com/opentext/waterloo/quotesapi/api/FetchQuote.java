8fc9b114ac108cb15b6834fcc2b48742abb03d63
a2zain <a2zain@uwaterloo.ca>
2021-01-28 00:11:01 -0500

    Fetch API

---
+++ src/main/java/com/opentext/waterloo/quotesapi/api/FetchQuote.java
@@ -0,0 +1,7 @@
+package com.opentext.waterloo.quotesapi.api;
+
+import org.json.JSONObject;
+
+public interface FetchQuote {
+    JSONObject connect() throws Exception;
+}
--- src/main/java/com/opentext/waterloo/quotesapi/api/QuoteController.java
+++ src/main/java/com/opentext/waterloo/quotesapi/api/QuoteController.java
@@ -1,5 +1,9 @@
 package com.opentext.waterloo.quotesapi.api;
 
+import com.opentext.waterloo.quotesapi.QuotesApiApplication;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.opentext.waterloo.quotesapi.model.Quote;
 import com.opentext.waterloo.quotesapi.service.QuoteService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,16 +11,31 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @RequestMapping(path = "api/v1/quotes")
 @RestController
 public class QuoteController {
-
+    private static final Logger log = LoggerFactory.getLogger(QuotesApiApplication.class);
     private final QuoteService quoteService;
+    @Autowired
+    private final FetchQuote localFetch;
+    @Autowired
+    private final FetchQuote remoteFetch;
 
     @Autowired
-    public QuoteController(QuoteService quoteService) {
+    public QuoteController(QuoteService quoteService, FetchQuote localFetch, FetchQuote remoteFetch) {
         this.quoteService = quoteService;
+        this.localFetch = localFetch;
+        this.remoteFetch = remoteFetch;
+    }
+
+    public JSONObject remoteConnect() throws Exception {
+        return remoteFetch.connect();
+    }
+    public JSONObject localConnect() throws Exception {
+        return localFetch.connect();
     }
 
     @PostMapping
@@ -24,9 +43,29 @@ public class QuoteController {
         quoteService.addQuote(quote);
     }
 
-    @GetMapping
-    public List<Quote> quotes() {
-        return quoteService.quotes();
+    @PostMapping
+    public void incrementLikes(@RequestBody Boolean like) {
+        quoteService.incrementLikes(like);
+    }
+
+    @GetMapping(path = "{date}")
+    public Quote quotes() throws Exception {
+        JSONObject json;
+        //try to fetch online api quote
+        try {
+            log.info("Fetching from theysaidso");
+            json = remoteConnect();
+        } catch (Exception e) { //failed, try to fetch locally stored json
+            log.error("Live quote fetch failed! Returning local json file" + e.getMessage());
+            json = localConnect();
+        }
+        JSONObject quote = new JSONObject(json.getJSONObject("contents").getJSONArray("quotes").getString(0));
+
+        String quoteOfTheDay = quote.get("quote").toString();
+        String timestamp = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss").format(LocalDateTime.now());
+
+        Quote result = new Quote();
+        return result;
     }
 
 //    @GetMapping(path = "current?date={date}")

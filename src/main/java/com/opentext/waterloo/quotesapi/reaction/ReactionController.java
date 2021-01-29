0b14da8f6d58fd37391fe44171a2764d1808b431
ketan <ketan@opentext.com>
2021-01-29 16:09:32 -0500

    pulled

--- src/main/java/com/opentext/waterloo/quotesapi/reaction/ReactionController.java
+++ src/main/java/com/opentext/waterloo/quotesapi/reaction/ReactionController.java
@@ -1,14 +1,9 @@
 package com.opentext.waterloo.quotesapi.reaction;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class ReactionController {
 
-    @RequestMapping
-    public String hello() {
-        return "hello";
-    }
 }

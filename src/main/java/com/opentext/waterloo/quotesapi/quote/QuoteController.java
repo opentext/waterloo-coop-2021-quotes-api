3246bc1e12bf23143adce8da9af8df93a7a1ecee
ketan <ketan@opentext.com>
2021-01-29 16:13:23 -0500

    added requestmapping

--- src/main/java/com/opentext/waterloo/quotesapi/quote/QuoteController.java
+++ src/main/java/com/opentext/waterloo/quotesapi/quote/QuoteController.java
@@ -3,7 +3,9 @@ package com.opentext.waterloo.quotesapi.quote;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping(path = "api/v1/quotes")
 @Controller
 public class QuoteController {
     @Autowired
--- src/main/java/com/opentext/waterloo/quotesapi/reaction/ReactionController.java
+++ src/main/java/com/opentext/waterloo/quotesapi/reaction/ReactionController.java
@@ -2,7 +2,9 @@ package com.opentext.waterloo.quotesapi.reaction;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping(path = "api/v1/reactions")
 @Controller
 public class ReactionController {
 

25b3eaa6797bdd62cae3c2702e062d694da5da28
ketan <ketan@opentext.com>
2021-01-29 17:13:59 -0500

    tables generate automatically

--- src/main/java/com/opentext/waterloo/quotesapi/quote/QuoteController.java
+++ src/main/java/com/opentext/waterloo/quotesapi/quote/QuoteController.java
@@ -3,15 +3,30 @@ package com.opentext.waterloo.quotesapi.quote;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RequestMapping(path = "api/v1/quotes")
 @Controller
 public class QuoteController {
     @Autowired
     private QuoteRepository quoteRepository;
+    private final QuoteService quoteService;
 
 
+    public QuoteController(QuoteRepository quoteRepository, QuoteService quoteService) {
+        this.quoteRepository = quoteRepository;
+        this.quoteService = quoteService;
+    }
+
+    @GetMapping
+    public List<Quote> getQuote(){
+        return quoteService.findAll();
+    }
 
 
 }

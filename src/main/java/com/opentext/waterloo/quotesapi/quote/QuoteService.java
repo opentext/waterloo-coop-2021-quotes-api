440d9f2e406059463c3b4a36bc72ed5263781e50
a2zain <a2zain@uwaterloo.ca>
2021-02-02 15:22:11 -0500

    fetch fix

--- src/main/java/com/opentext/waterloo/quotesapi/quote/QuoteService.java
+++ src/main/java/com/opentext/waterloo/quotesapi/quote/QuoteService.java
@@ -1,6 +1,11 @@
 package com.opentext.waterloo.quotesapi.quote;
 
+import com.opentext.waterloo.quotesapi.QuotesApiApplication;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.text.ParseException;
@@ -12,8 +17,22 @@ import java.util.UUID;
 @Service
 public class QuoteService {
 
+    private static final Logger log = LoggerFactory.getLogger(QuotesApiApplication.class);
+
     @Autowired
     QuoteRepository quoteRepository;
+
+    @Autowired
+    private FetchQuote localFetch;
+    @Autowired
+    private FetchQuote remoteFetch;
+
+    public Quote remoteConnect() throws Exception {
+        return remoteFetch.connect();
+    }
+    public Quote localConnect() throws Exception {
+        return localFetch.connect();
+    }
 
     public Quote getQuoteByUUID(UUID uuid) {
         Optional<Quote> quote = quoteRepository.findById(uuid);
@@ -42,5 +61,19 @@ public class QuoteService {
 
     public List<Quote> getQuotes() {
         return quoteRepository.findAll();
+    }
+
+    @Scheduled(cron = "*/5 * * * *")
+    public void saveQuote() throws Exception {
+        Quote quote;
+        try {
+            log.info("Fetching from theysaidso");
+            quote = remoteConnect();
+        } catch (Exception e) {
+            log.error("Live quote fetch failed! Returning local json file" + e.getMessage());
+            quote = localConnect();
+        }
+
+        quoteRepository.save(quote);
     }
 }
